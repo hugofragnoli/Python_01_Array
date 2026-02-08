d2b2206cf847a65da02e86035b2ca7f78d6b46c0
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-08 17:51:54 +0100

    Initial commit -> ex00 Python 01

---
+++ ex00/give_bmi.py
@@ -0,0 +1,19 @@
+# 2. Formule MathématiquePour rappel, la formule de 
+# l'IMC est :$$BMI = \frac{poids}{taille^2}$$Le poids est en kg et la taille en mètres.
+import numpy as np
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    i = 0
+
+
+
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    i = 0
+
+
+
+if __name__ == "__main__":
+    main()
