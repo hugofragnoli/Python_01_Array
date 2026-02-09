9885f37874cb6d2e829235d4178872a8eb4f7508
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-09 01:27:48 +0100

    + debut ex01 array2d.py ++

--- ex01/array2d.py
+++ ex01/array2d.py
@@ -1,5 +1,12 @@
+import numpy as np
+
+
 def slice_me(family: list, start: int, end: int) -> list:
     if end < start:
         tmp = end
         end = start
         start = tmp
+    np_family = np.array(family)
+
+    res = np_family.tolist()
+    
