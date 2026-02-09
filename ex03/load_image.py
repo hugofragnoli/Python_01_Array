2ca2c0e8f5e525062cbc139bc656e87949fb3dcf
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-09 17:15:54 +0100

    hihi

--- ex03/load_image.py
+++ ex03/load_image.py
@@ -1,6 +1,5 @@
 from PIL import Image
 from numpy import array
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -10,9 +9,9 @@ import os
 # matplotlib biblio de dessin Python. Transforme des données
 # numériques en une fenetre visuelle avec axes gradés
 
+
 def ft_load(path: str) -> array:
     try:
-        path = "animal.jpeg"
         if not os.path.exists(path):
             raise FileNotFoundError(f"No such file or directory: {path}")
         _, extension = os.path.splitext(path)
@@ -28,9 +27,3 @@ def ft_load(path: str) -> array:
     except Exception as e:
         print(f"Error: {e}")
         return None
-
-def display_image_info(path: str):
-    img_array = ft_load(path)
-
-    if img_array is not None:
-        print(img_array)
--- ex03/zoom.py
+++ ex03/zoom.py
@@ -1,5 +1,34 @@
-from load_image import display_image_info
 from load_image import ft_load
+import matplotlib.pyplot as plt
+from numpy import array
 
-def ft_zoom(img_array) -> int:
-    zoomed_img = img_array[100:500, 400:800, :]
+
+def ft_zoom(path: str) -> array:
+    # ici on dit :
+    # Prends les pixels a partir de la ligne 100
+    # jusqua ligne 500 (hauteur)
+    # Prends de colonne 400 a 800 (largeur)
+    # :1 on dit a Python de garder 1 canal pour le gris
+    try:
+        img = ft_load(path)
+        if img is None:
+            return None
+        print(img)
+        zoomed_img = img[100:800, 150:800, :1]
+
+        print(f"New shape after slicing: {zoomed_img.shape}")
+        print(zoomed_img)
+
+        plt.imshow(zoomed_img, cmap="gray")
+        plt.title("Zoomed animal.jpeg")
+        plt.show()
+
+        return zoomed_img
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    ft_zoom("animal.jpeg")
