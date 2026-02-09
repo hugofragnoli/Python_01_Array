4ed167c385f14ca224904a0a83329d9c12acec82
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-09 01:42:14 +0100

    add index numpy pour slice

--- ex01/array2d.py
+++ ex01/array2d.py
@@ -2,12 +2,17 @@ import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
-    if end < start:
-        tmp = end
-        end = start
-        start = tmp
-    np_family = np.array(family)
-    np_family = np.reshape(np_family, start, end)
 
-    return np_family.tolist()
+    np_family = np.array(family)
+
+    print(f"My shape is : {np_family.shape}")
+
+    res = np_family[start:end]
+    # fct reshape peut changer la taille dun array instant'
+    # ici on res devient un tableau numpy avec sstart inclus pour debut
+    # et end exclus
+
+    print(f"My new shape is : {res.shape}")
+
+    return res.tolist()
     
